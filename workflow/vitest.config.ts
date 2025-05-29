14aa15a9f9c5bdb30643dcf5bbb7e0dd6ce4c8dc
Elliott deLaunay <edelauna@gmail.com>
2025-05-29 02:29:28 +0000

    fix(crypto): generating new key on the fly

--- workflow/vitest.config.ts
+++ workflow/vitest.config.ts
@@ -5,6 +5,18 @@ export default defineWorkersConfig(async () => {
 	// Read all migrations in the `migrations` directory
 	const migrationsPath = path.join(__dirname, "migrations");
 	const migrations = await readD1Migrations(migrationsPath);
+
+	const key = await crypto.subtle.generateKey(
+		{
+			name: 'RSA-OAEP',
+			modulusLength: 2048, // Or a different size
+			publicExponent: new Uint8Array([1, 0, 1]), // Equivalent to 65537
+			hash: 'SHA-256', // Specify the hash algorithm
+		},
+		true, // extractable
+		['encrypt', 'decrypt']
+	) as CryptoKeyPair
+
 	return {
 		test: {
 			setupFiles: ["./test/setup.ts"],
@@ -14,8 +26,12 @@ export default defineWorkersConfig(async () => {
 					singleWorker: true,
 					wrangler: { configPath: './wrangler.toml' },
 					miniflare: {
-						// Add a test-only binding for migrations, so we can apply them in a setup file
-						bindings: { TEST_MIGRATIONS: migrations },
+						bindings: {
+							// Add a test-only binding for migrations, so we can apply them in a setup file
+							TEST_MIGRATIONS: migrations,
+							// Generating a key for tests
+							RSA_PRIVATE_KEY: JSON.stringify(await crypto.subtle.exportKey('jwk', key.privateKey))
+						},
 					},
 				},
 			},
